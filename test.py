575e840aec94bd0b121a71585fc0e82ea50db52d
lafengnan <bluerwf@gmail.com>
2014-12-04 22:15:06 +0800

    Add file walker function

--- test.py
+++ test.py
@@ -10,21 +10,36 @@ def main():
     print 'ok?'
     if sys.argv[1] == '-ls':
         path = sys.argv[2]
-        getfilelist(path)
+        fl = getfilelist(path, '.py')
+        print fl
+        for f in fl:
+            with open(f) as fh:
+                content = fh.readlines()
+                for line in content:
+                    print line,
     elif sys.argv[1]  == 'lt':
         getname()
     else:
         print 'nothing'
 
-def getfilelist(path):
+def getfilelist(path,suffix='*'):
     if pll.exists(path):
-      print  os.listdir(path) 
-
+        #print os.listdir(path)
+        w = os.walk(path)
+        flist = list()
+        while True:
+            try:
+                ufo = w.next()
+                files = ufo[2] if suffix == '*' else [f for f in ufo[2] if f.endswith(suffix)]
+                for i in files:
+                     flist.append(os.path.join(ufo[0],i))
+            except StopIteration as e:
+                break
+        return flist
     else:
         print "invaild path"
 
-
-    print getfilelist.__name__
+    #print getfilelist.__name__
     
 def getname():
     print getname.__name__

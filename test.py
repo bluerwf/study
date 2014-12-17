937eaa875965811362515eaccdf05b81a208608e
lafengnan <panzhongbin@gmail.com>
2014-12-17 22:24:47 +0800

    rename class.py to file_class.py

--- test.py
+++ test.py
@@ -2,15 +2,13 @@
 # coding=utf-8
 
 import sys
-from os import path as pll 
-import os
-print sys.argv
+from file_class import File
 
 def main():
-    print 'ok?'
+    path = sys.argv[2]
+    fh = File(path, '.py')
     if sys.argv[1] == 'ls':
-        path = sys.argv[2]
-        fl = getfilelist(path, '.py')
+        fl = fh.filelist
         print fl
         for f in fl:
             with open(f) as fh:
@@ -18,50 +16,10 @@ def main():
                 for line in content:
                     print line,
     elif sys.argv[1]  == 'ld':
-        dirlist = getfilelist(sys.argv[2])
+        dirlist = fh.getfilelist(path)
         print dirlist
     else:
         print 'nothing'
-
-def getfilelist(path,suffix='*'):
-    if pll.exists(path):
-        #print os.listdir(path)
-        w = os.walk(path)
-        flist = list()
-        while True:
-            try:
-                ufo = w.next()
-                files = ufo[2] if suffix == '*' else [f for f in ufo[2] if f.endswith(suffix)]
-                for i in files:
-                     flist.append(os.path.join(ufo[0],i))
-            except StopIteration as e:
-                break
-        return flist
-    else:
-        print "invaild path"
-
-    #print getfilelist.__name__
-def getdirlist(path):
-    if pll.exists(path):
-        #print os.listdir(path)
-        w = os.walk(path)
-        dirlist = list()
-        while True:
-            try:
-                ufo = w.next()
-                dirs = ufo[1] 
-                for i in dirs:
-                    dirlist.append(os.path.join(ufo[0],i))
-            except StopIteration as e:
-                break
-        return dirlist 
-    else:
-        print "invaild path"
-
-        
-    
-def getname():
-    print getname.__name__
 
 if '__main__' == __name__:
     main()
